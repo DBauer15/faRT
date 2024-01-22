9bd73de28a5b5530504d15c5b9739b279367ba60
David Bauer <david.bauer009@gmail.com>
2024-01-22 13:17:09 -0800

    Updated SAH heuristic and cleaned up BVH implementation

--- src/opengl/bvh.cpp
+++ src/opengl/bvh.cpp
@@ -7,7 +7,35 @@
 
 namespace fart {
 
-BVH::BVH(std::vector<Geometry>& geometries) {
+Bounds
+Bounds::merge(const Bounds& other) const {
+    // Inline min/max faster than glm and std equivalents
+    return {
+        {   
+            min.x > other.min.x ? other.min.x : min.x, 
+                min.y > other.min.y ? other.min.y : min.y, 
+                min.z > other.min.z ? other.min.z : min.z, 
+        },
+            {   
+                max.x < other.max.x ? other.max.x : max.x,
+                max.y < other.max.y ? other.max.y : max.y,
+                max.z < other.max.z ? other.max.z : max.z,
+            }
+    };
+}
+
+void
+Bounds::extend(const glm::vec3& other) {
+    // Inline min/max faster than glm and std equivalents
+    min.x = min.x > other.x ? other.x : min.x;
+    min.y = min.y > other.y ? other.y : min.y;
+    min.z = min.z > other.z ? other.z : min.z;
+    max.x = max.x < other.x ? other.x : max.x;
+    max.y = max.y < other.y ? other.y : max.y;
+    max.z = max.z < other.z ? other.z : max.z;
+}
+
+BVH::BVH(std::vector<Geometry>& geometries, BVHSplitMethod split_method) {
     size_t index_offset = 0;
     for (auto& geometry : geometries) {
         m_vertices.reserve(m_vertices.size() + geometry.vertices.size());
@@ -20,31 +48,34 @@ BVH::BVH(std::vector<Geometry>& geometries) {
         });
         index_offset += geometry.vertices.size();
     }
+    m_split_method = split_method;
 
-    build(m_vertices, m_indices);
+    build();
 }
 
-BVH::BVH(std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices) {
+BVH::BVH(std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices, BVHSplitMethod split_method) {
     m_vertices = vertices;
     m_indices = indices;
-    build(m_vertices, m_indices);
+    m_split_method = split_method;
+
+    build();
 }
 
 void
-BVH::build(std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices) {
+BVH::build() {
     WARN("Building BVH.. This may take a while.");
     auto t_start = std::chrono::high_resolution_clock::now();
         
-    size_t N = indices.size() / 3;
+    size_t N = m_indices.size() / 3;
     m_nodes_used = 1;
     m_centroids.resize(N);
     m_bvh_nodes.resize( N * 2 - 1 );
 
     for (size_t i = 0; i < N; ++i) {
         
-        const glm::vec3& t1 = vertices[indices[3*i]].position; //glm::make_vec3(&vertices[3*indices[3*i]]);
-        const glm::vec3& t2 = vertices[indices[3*i+1]].position; //glm::make_vec3(&vertices[3*indices[3*i+1]]);
-        const glm::vec3& t3 = vertices[indices[3*i+2]].position; //glm::make_vec3(&vertices[3*indices[3*i+2]]);
+        const glm::vec3& t1 = m_vertices[m_indices[3*i]].position;
+        const glm::vec3& t2 = m_vertices[m_indices[3*i+1]].position;
+        const glm::vec3& t3 = m_vertices[m_indices[3*i+2]].position;
 
         m_centroids[i] = (t1 + t2 + t3) / 3.f;
     }
@@ -54,8 +85,8 @@ BVH::build(std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices)
     root.left_child = 0;
     root.first_tri_index_id = 0, root.tri_count = N;
 
-    updateNodeBounds( root_idx, vertices, indices );
-    subdivide( root_idx, vertices, indices );
+    updateNodeBounds( root_idx );
+    subdivide( root_idx );
 
     auto build_time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - t_start);
 
@@ -63,31 +94,33 @@ BVH::build(std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices)
 }
 
 void
-BVH::updateNodeBounds( uint32_t node_idx, std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices) {
+BVH::updateNodeBounds( uint32_t node_idx ) {
     BVHNode& node = m_bvh_nodes[node_idx];
     node.aabb.min = glm::vec3(1e30f);
     node.aabb.max = glm::vec3(-1e30f);
 
     for (uint32_t i = node.first_tri_index_id; i < node.first_tri_index_id + 3 * node.tri_count; i++) {
-        const glm::vec3& t1 = vertices[indices[i]].position;
+        const glm::vec3& t1 = m_vertices[m_indices[i]].position;
         node.aabb.extend(t1);
     }
 }
 
 void
-BVH::subdivide( uint32_t node_idx, std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices) {
+BVH::subdivide( uint32_t node_idx ) {
 
-    BVHNode& node = m_bvh_nodes[node_idx];
-
-    glm::vec3 extent = node.aabb.extent();
-
-    int axis = 0;
-    if (extent.y > extent.x) axis = 1;
-    if (extent.z > extent[axis]) axis = 2;
+    uint32_t axis = 0;
 
     float split_pos;
-    if (!splitSAH(axis, node_idx, vertices, indices, split_pos)) return;
+    switch (m_split_method) {
+        case BVHSplitMethod::Equal: 
+            if (!splitEqual(node_idx, split_pos, axis)) return;
+            break;
+        case BVHSplitMethod::SAH:
+            if (!splitSAH(node_idx, split_pos, axis)) return;
+            break;
+    }
 
+    BVHNode& node = m_bvh_nodes[node_idx];
     uint32_t i = node.first_tri_index_id;
     uint32_t j = i + (node.tri_count - 1)*3;
     while (i <= j) {
@@ -95,9 +128,9 @@ BVH::subdivide( uint32_t node_idx, std::vector<AligendVertex>& vertices, std::ve
         if (m_centroids[i/3][axis] < split_pos) {
             i+=3;
         } else {
-            std::swap(indices[i], indices[j]);
-            std::swap(indices[i+1], indices[j+1]);
-            std::swap(indices[i+2], indices[j+2]);
+            std::swap(m_indices[i], m_indices[j]);
+            std::swap(m_indices[i+1], m_indices[j+1]);
+            std::swap(m_indices[i+2], m_indices[j+2]);
             std::swap(m_centroids[i/3], m_centroids[j/3]);
             j-=3;
         }
@@ -118,17 +151,20 @@ BVH::subdivide( uint32_t node_idx, std::vector<AligendVertex>& vertices, std::ve
 
     node.tri_count = 0;
 
-    updateNodeBounds( left_child_idx, vertices, indices );
-    updateNodeBounds( right_child_idx, vertices, indices );
+    updateNodeBounds( left_child_idx );
+    updateNodeBounds( right_child_idx );
 
-    subdivide( left_child_idx, vertices, indices );
-    subdivide( right_child_idx, vertices, indices );
+    subdivide( left_child_idx );
+    subdivide( right_child_idx );
 }
 
 bool
-BVH::splitCentroid( uint32_t axis, uint32_t node_idx, std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices, float& split_pos) {
+BVH::splitEqual(uint32_t node_idx, float& split_pos, uint32_t& axis) {
     BVHNode& node = m_bvh_nodes[node_idx];
     glm::vec3 extent = node.aabb.extent();
+    axis = 0;
+    if (extent.y > extent.x) axis = 1;
+    if (extent.z > extent[axis]) axis = 2;
     split_pos = node.aabb.min[axis] + extent[axis] * 0.5f;
 
     return node.tri_count > 2;
@@ -140,53 +176,64 @@ BVH::splitCentroid( uint32_t axis, uint32_t node_idx, std::vector<AligendVertex>
  *
  */
 bool
-BVH::splitSAH( uint32_t axis, uint32_t node_idx, std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices, float& split_pos) {
+BVH::splitSAH(uint32_t node_idx, float& split_pos, uint32_t& axis) {
+
     const BVHNode& node = m_bvh_nodes[node_idx];
+    const glm::vec3 node_extent = node.aabb.extent();
     if (node.tri_count <= 2) return false;
+
     constexpr int n_buckets = 12;
-    BVHSplitBucket buckets[n_buckets];
-
-    for (uint32_t i = node.first_tri_index_id; i < node.first_tri_index_id + 3 * node.tri_count; i+=3) {
-        int b = n_buckets * ((m_centroids[i/3][axis] - node.aabb.min[axis]) / node.aabb.extent()[axis]);
-        if (b == n_buckets) b = n_buckets - 1;
-        buckets[b].count += 1;
-        buckets[b].bounds.extend(vertices[indices[i]].position);
-        buckets[b].bounds.extend(vertices[indices[i+1]].position);
-        buckets[b].bounds.extend(vertices[indices[i+2]].position);
-    }
-
     constexpr int n_splits = n_buckets - 1;
-    float costs[n_splits] = {};
-    int count_below = 0;
-    Bounds bound_below;
-    for (int i = 0; i < n_splits; i++) {
-        bound_below = bound_below.merge(buckets[i].bounds);
-        count_below += buckets[i].count;
-        costs[i] += count_below * std::max(0.f, bound_below.surfaceArea());
-    }
-
-    int count_above = 0;
-    Bounds bound_above;
-    for (int i = n_splits; i >= 1; i--) {
-        bound_above = bound_above.merge(buckets[i].bounds);
-        count_above += buckets[i].count;
-        costs[i - 1] += count_above * std::max(0.f, bound_above.surfaceArea());
-    }
 
     int min_cost_split_bucket = -1;
+    uint32_t min_split_axis = 0;
     float min_cost = std::numeric_limits<float>::infinity();
-    for (int i = 0; i < n_splits; i++) {
-        if (costs[i] < min_cost) {
-            min_cost = costs[i];
-            min_cost_split_bucket = i;
-        }
-    }
 
+    for (size_t ax = 0; ax < 3; ax++) {
+        if (node_extent[ax] <= 0.f) continue;
+        BVHSplitBucket buckets[n_buckets];
+
+        for (uint32_t i = node.first_tri_index_id; i < node.first_tri_index_id + 3 * node.tri_count; i+=3) {
+            int b = n_buckets * ((m_centroids[i/3][ax] - node.aabb.min[ax]) / node_extent[ax]);
+            if (b == n_buckets) b = n_buckets - 1;
+            buckets[b].count += 1;
+            buckets[b].bounds.extend(m_vertices[m_indices[i]].position);
+            buckets[b].bounds.extend(m_vertices[m_indices[i+1]].position);
+            buckets[b].bounds.extend(m_vertices[m_indices[i+2]].position);
+        }
+
+        float costs[n_splits] = {};
+        int count_below = 0;
+        Bounds bound_below;
+        for (int i = 0; i < n_splits; i++) {
+            bound_below = bound_below.merge(buckets[i].bounds);
+            count_below += buckets[i].count;
+            costs[i] += count_below * bound_below.surfaceArea();
+        }
+
+        int count_above = 0;
+        Bounds bound_above;
+        for (int i = n_splits; i >= 1; i--) {
+            bound_above = bound_above.merge(buckets[i].bounds);
+            count_above += buckets[i].count;
+            costs[i - 1] += count_above * bound_above.surfaceArea();
+        }
+
+        for (int i = 0; i < n_splits; i++) {
+            if (costs[i] < min_cost) {
+                min_cost = costs[i];
+                min_cost_split_bucket = i;
+                min_split_axis = ax;
+            }
+        }
+
+    }
 
     float leaf_cost = node.tri_count;
     min_cost = 1.f / 2.f + min_cost / node.aabb.surfaceArea();
 
-    split_pos = node.aabb.min[axis] + (node.aabb.extent()[axis] / n_buckets) * (min_cost_split_bucket + 1);
+    axis = min_split_axis;
+    split_pos = node.aabb.min[axis] + (node_extent[axis] / n_buckets) * (min_cost_split_bucket + 1);
     
     return node.tri_count > 16 || min_cost < leaf_cost;
 }
--- src/opengl/bvh.h
+++ src/opengl/bvh.h
@@ -1,11 +1,10 @@
 #pragma once
 
-#include <cstdint>
+#include "common/mesh.h"
+
 #include <vector>
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
-
-#include "common/mesh.h"
 
 namespace fart {
 
@@ -13,24 +12,12 @@ struct Bounds {
     alignas(16) glm::vec3 min { 1e30f };
     alignas(16) glm::vec3 max { -1e30f };
 
-    Bounds merge(const Bounds& other) {
-        return {
-            { std::min(min.x, other.min.x), std::min(min.y, other.min.y), std::min(min.z, other.min.z) },
-            { std::max(max.x, other.max.x), std::max(max.y, other.max.y), std::max(max.z, other.max.z) }
-        };
-    }
+    Bounds merge(const Bounds& other) const;
 
-    void extend(const glm::vec3& other) {
-        min.x = std::min(min.x, other.x);
-        min.y = std::min(min.y, other.y);
-        min.z = std::min(min.z, other.z);
-        max.x = std::max(max.x, other.x);
-        max.y = std::max(max.y, other.y);
-        max.z = std::max(max.z, other.z);
-    }
+    void extend(const glm::vec3& other);
 
     glm::vec3 extent() const {
-        return max - min;
+        return { max.x - min.x, max.y - min.y, max.z - min.z };
     }
 
     float surfaceArea() const {
@@ -40,35 +27,41 @@ struct Bounds {
     }
 };
 
-struct BVHNode {
-    public:
-        Bounds aabb;
-        uint32_t left_child { 0 };
-        uint32_t first_tri_index_id, tri_count, filler;
-};
-
 struct BVHSplitBucket {
     int count = 0;
     Bounds bounds;
 };
 
+struct BVHNode {
+    Bounds aabb;
+    uint32_t left_child { 0 };
+    uint32_t first_tri_index_id, tri_count, filler;
+};
+
+enum BVHSplitMethod {
+    Equal,
+    SAH,
+};
+
 struct BVH {
 
     public:
-        BVH(std::vector<Geometry>& geometries);
-        BVH(std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices);
+        BVH( std::vector<Geometry>& geometries, BVHSplitMethod split_method = BVHSplitMethod::SAH );
+        BVH( std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices, BVHSplitMethod split_method = BVHSplitMethod::SAH );
+
         size_t getNodesUsed() { return m_nodes_used; }
         std::vector<BVHNode>& getNodes() { return m_bvh_nodes; }
         std::vector<AligendVertex>& getVertices() { return m_vertices; }
         std::vector<uint32_t>& getIndices() { return m_indices; }
 
     private:
-        void build( std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices);
-        void updateNodeBounds( uint32_t node_idx, std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices);
-        void subdivide( uint32_t node_idx, std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices);
-        bool splitCentroid( uint32_t axis, uint32_t node_idx, std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices, float& split_pos);
-        bool splitSAH( uint32_t axis, uint32_t node_idx, std::vector<AligendVertex>& vertices, std::vector<uint32_t>& indices, float& split_pos);
+        void build();
+        void updateNodeBounds( uint32_t node_idx );
+        void subdivide( uint32_t node_idx );
+        bool splitEqual(uint32_t node_idx, float& split_pos, uint32_t& axis );
+        bool splitSAH(uint32_t node_idx, float& split_pos, uint32_t& axis );
 
+        BVHSplitMethod m_split_method;
         std::vector<AligendVertex> m_vertices;
         std::vector<uint32_t> m_indices;
 
