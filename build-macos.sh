1425dd3fc7bdfcb157fd456bce6f606db14741bf
David Bauer <david.bauer009@gmail.com>
2024-02-26 10:28:15 -0800

    Update build process

---
+++ build-macos.sh
@@ -0,0 +1,8 @@
+cmake -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -DBUILD_OPENGL_RENDERER=OFF \
+    -DBUILD_METAL_RENDERER=ON \
+    .
+cmake --build build -j
+mv build/compile_commands.json ./
