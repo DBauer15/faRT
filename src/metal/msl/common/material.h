e6e91c8f13227840a0541f0c54be0fa86d599fe0
David Bauer <davbauer@ucdavis.edu>
2024-03-14 20:41:27 -0700

    Dielectrics for MSL (possibly broken)

--- src/metal/msl/common/material.h
+++ src/metal/msl/common/material.h
@@ -35,17 +35,39 @@ float ggxShadowing(float roughness, float cos_theta_h) {
 }
 
 float pdf_ggx(thread const SurfaceInteraction    &si, 
-                 float3                             w_i, 
-                 float3                             w_o) 
+              float3                             w_i, 
+              float3                             w_o) 
 {
     float theta_i = dot(si.n, w_i);
     if (theta_i < 0.f) return 0.f;
 
     float3 h = normalize(w_i + w_o);
     float ndoth = dot(si.n, h);
-    float D = ggxShadowing(si.mat.specular_roughness, ndoth);
+    float G = ggxShadowing(si.mat.specular_roughness, ndoth);
 
-    return D * ndoth / max(FLT_MIN, 4 * dot(w_o, h));
+    return G * ndoth / max(FLT_MIN, 4 * dot(w_o, h));
+}
+
+float pdf_ggx_transmission(thread const SurfaceInteraction  &si,
+                           float3                           w_i,
+                           float3                           w_o)
+{
+    float ldotn = dot(-si.n, w_i);
+    if (ldotn < 0.f) return 0.f;
+    float ior_from = 1.f;
+    float ior_to = si.mat.specular_ior;
+    if (!si.front_face) {
+        ior_from = ior_to;
+        ior_to = 1.f;
+    }
+    float3 h = normalize(-(ior_to * w_i + ior_from * w_o));
+    float ndoth = abs(dot(si.n, h));
+    float ldoth = abs(dot(w_i, h));
+    float vdoth = abs(dot(w_o, h));
+    float G = ggxShadowing(si.mat.specular_roughness, ndoth);
+
+    return (ldoth * G) / (ldotn * ndoth);
+
 }
 
 float3 sample_ggx(thread const SurfaceInteraction    &si, 
@@ -56,29 +78,79 @@ float3 sample_ggx(thread const SurfaceInteraction    &si,
     return w;
 }
 
+float3 sample_ggx_transmission(thread const SurfaceInteraction   &si,
+                              thread RNG                        &rng)
+{
+    float3 h = randomGGXMicrofacet(next_random2f(rng), si.n, si.mat.specular_roughness);
+    
+    float ior = si.mat.specular_ior;
+    if (si.front_face)
+        ior = 1.f / ior;
+    float3 w = refract(-si.w_o, h, ior);
+
+    return w;
+}
+
 float3 eval_ggx(thread const SurfaceInteraction   &si,
                 float3                            w_i,
                 float3                            w_o,
+                float3                            w_h,
                 float3                            f0)
 {
-    float3 h = normalize(w_i + w_o);
-    float hdotl = max(0.f, dot(w_i, h));
+    float hdotl = max(0.f, dot(w_i, w_h));
     float ndotv = max(FLT_MIN, dot(si.n, w_o));
     float ndotl = max(FLT_MIN, dot(si.n, w_i));
-    float ndoth = max(0.f, dot(si.n, h));
+    float ndoth = max(0.f, dot(si.n, w_h));
 
     // fresnel
     float3 F = si.mat.specular_weight * si.mat.specular_color * fresnelSchlick(f0, hdotl);
     
     // geometric
-    float G = ggxGeomtric(si.mat.specular_roughness, ndotl, ndotv);
+    float D = ggxGeomtric(si.mat.specular_roughness, ndotl, ndotv);
 
     // shadowing
-    float D = ggxShadowing(si.mat.specular_roughness, ndoth);
+    float G = ggxShadowing(si.mat.specular_roughness, ndoth);
 
     return (F * G * D) / (4.f * ndotv /* * ndotl */) /* * ndotl */; // ndotl (theta_i) left out for numerical robustness
 }
 
+float3 eval_ggx_transmission(thread const SurfaceInteraction    &si,
+                             float3                             w_i,
+                             float3                             w_o,
+                             float3                             w_h,
+                             float3                             f0)
+{
+    float3 n = si.front_face ? si.n : -si.n;
+    float hdotl = abs(dot(w_i, w_h));
+    float hdotv = abs(dot(w_o, w_h));
+    float ndotv = max(FLT_MIN, abs(dot(n, w_o)));
+    float ndotl = max(FLT_MIN, abs(dot(n, w_i)));
+    float ndoth = max(FLT_MIN, abs(dot(n, w_h)));
+
+    float ior_from = 1.f;
+    float ior_to = si.mat.specular_ior;
+    if (!si.front_face) {
+        ior_from = ior_to;
+        ior_to = 1.f;
+    }
+
+    // fresnel
+    float3 F = si.mat.specular_weight * si.mat.specular_color * fresnelSchlick(f0, hdotl);
+    
+    // geometric
+    float D = ggxGeomtric(si.mat.specular_roughness, ndotl, ndotv);
+
+    // shadowing
+    float G = ggxShadowing(si.mat.specular_roughness, ndoth);
+
+    float term0 = ((hdotl * hdotv) / (ndotl * ndotv)); 
+    float3 term1 = (ior_from * ior_from) * (float(1.f) - F) * G * D;
+    float term2 = pow(ior_to * dot(w_h, w_i) + ior_from * dot(w_h, w_o), 2);
+
+    return term0 * term1 * ndotl;
+
+    return ndotl * term0 * term1 / term2;
+}
 
 float pdf_lambert(thread const SurfaceInteraction   &si, 
                   float3                            w_i, 
@@ -111,7 +183,10 @@ float3 eval_metal(thread const SurfaceInteraction   &si,
     if (si.mat.base_color_texid >= 0)
         f0 = si.textures[si.mat.base_color_texid].texture.sample(sam, si.uv).rgb;
     f0 = f0 * si.mat.base_weight;
-    return eval_ggx(si, w_i, w_o, f0);
+    
+    // half angle vector
+    float3 h = normalize(w_i + w_o);
+    return eval_ggx(si, w_i, w_o, h, f0);
 }
 
 float3 eval_glossy(thread const SurfaceInteraction  &si, 
@@ -120,7 +195,30 @@ float3 eval_glossy(thread const SurfaceInteraction  &si,
 {
     // base reflectance
     float3 f0 = float3(1.f) * pow((1.f - si.mat.specular_ior) / (1.f + si.mat.specular_ior), 2.f);
-    return eval_ggx(si, w_i, w_o, f0);
+    
+    // half angle vector
+    float3 h = normalize(w_i + w_o);
+    return eval_ggx(si, w_i, w_o, h, f0);
+}
+
+float3 eval_glossy_transmission(thread const SurfaceInteraction &si,
+                                float3                          w_i,
+                                float3                          w_o)
+{
+    if (dot(si.n, w_i) > 0.f) return float3(0.f);
+    float ior_from = 1.f; // air
+    float ior_to = si.mat.specular_ior; // medium
+    if (!si.front_face) { // we are in the medium going out -> switch iors
+        ior_from = ior_to;
+        ior_to = 1.f;
+    } 
+    // float3 h = normalize(w_i + w_o * ior_from / ior_to);
+    float3 h = normalize(-(ior_to * w_i + ior_from * w_o));
+
+    // base reflectance
+    float3 f0 = float3(1.f) * pow((ior_from - ior_to) / (ior_from + ior_to), 2.f);
+
+    return si.mat.specular_color * eval_ggx_transmission(si, w_i, w_o, h, f0);
 }
 
 float3 eval_diffuse(thread const SurfaceInteraction     &si, 
@@ -158,10 +256,12 @@ float bsdf_pdf(const thread SurfaceInteraction    &si,
                float3                             w_i, 
                float3                             w_o) 
 {
+    float n_components = si.mat.transmission_weight > 0.f ? 3.f : 2.f;
     float diffuse = pdf_lambert(si, w_i, w_o);
     float glossy = pdf_ggx(si, w_i, w_o);
+    float transmission = si.mat.transmission_weight > 0.f ? pdf_ggx_transmission(si, w_i, w_o) : 0.f;
 
-    return (diffuse + glossy) / 2.f;
+    return (diffuse + glossy + transmission) / n_components;
 }
 
 float3 bsdf_sample(thread const SurfaceInteraction  &si, 
@@ -171,13 +271,18 @@ float3 bsdf_sample(thread const SurfaceInteraction  &si,
     float3 w;
 
     // TODO: Find better heuristic to choose the sampler
-    int bsdf_component = next_randomf(rng) * 2.f;
+    float n_components = si.mat.transmission_weight > 0.f ? 3.f : 2.f;
+    int bsdf_component = next_randomf(rng) * n_components;
+    // bsdf_component = 2;
     if (bsdf_component == 0) {
         w = sample_lambert(si, rng);
-    } else {
+    } else if (bsdf_component == 1) {
         w = sample_ggx(si, rng);
-    } 
+    } else {
+        w = sample_ggx_transmission(si, rng);
+    }
 
+    // pdf = pdf_ggx_transmission(si, w, si.w_o);
     pdf = bsdf_pdf(si, w, si.w_o);
     return w;
 }
@@ -187,12 +292,16 @@ float3 bsdf_eval(thread const SurfaceInteraction    &si,
                  float3                             w_o, 
                  thread RNG                         &rng)
 {
-    float3 E_specular = ggx_reflectance(si, w_o, rng);
-    float3 diffuse = eval_diffuse(si, w_i, w_o);
-    float3 glossy = eval_glossy(si, w_i, w_o);
-    float3 metal = eval_metal(si, w_i, w_o);
+    float3 glossy_transmission = si.mat.transmission_weight > 0.f ? eval_glossy_transmission(si, w_i, w_o) : 0.f;
+    // return glossy_transmission;
+    float3 glossy = si.mat.specular_weight > 0.f ? eval_glossy(si, w_i, w_o) : 0.f;
+    float3 E_glossy = si.mat.specular_weight > 0.f ? ggx_reflectance(si, w_o, rng) : 0.f;
+    float3 metal = si.mat.base_metalness > 0.f ? eval_metal(si, w_i, w_o) : 0.f;
 
-    float3 dielectric = glossy + (float3(1.f) - E_specular) * diffuse;
+    float3 diffuse = eval_diffuse(si, w_i, w_o);
+
+    float3 t_dielectric = mix(diffuse, glossy_transmission, si.mat.transmission_weight);
+    float3 dielectric = glossy + (float3(1.f) - E_glossy) * t_dielectric;
 
     return mix(dielectric, metal, si.mat.base_metalness);
 }
