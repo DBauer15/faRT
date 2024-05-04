f896f97e7939b7a62ac4d6d956e35386e52bcdb0
David Bauer <davbauer@ucdavis.edu>
2024-05-04 16:31:44 -0700

    minor

--- src/common/app.cpp
+++ src/common/app.cpp
@@ -7,7 +7,7 @@ namespace fart {
 App::App(std::string scene) {
     m_renderer = std::make_unique<DeviceRenderer>();
     Config config = {};
-    config.vertex_alignment = m_renderer->preferredAlignment();
+    config.vertex_alignment = m_renderer->preferredVertexAlignment();
     m_scene = std::make_shared<Scene>(scene, config);
     if (!m_scene->isValid()) return;
 
